e2a948f15851fdc0985b8ea794b48af3eca85b91
Themiya <66285748+tkuhemiya@users.noreply.github.com>
2026-05-07 10:42:09 +0530

    added nvim config (#1)

---
+++ nvim/README.md
@@ -0,0 +1,6 @@
+
+alias nvim-dev='NVIM_APPNAME=nvim-dev nvim'
+
+nvim-dev
+
+read https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
---
+++ nvim/lua/options.lua
@@ -0,0 +1,46 @@
+-- Leaders
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.swapfile = false
+
+-- Reload file when changed externally.
+vim.o.autoread = true
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.")
+  end,
+})
+
+vim.opt.mouse = "a"
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.undofile = true
+vim.opt.splitbelow = true
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+
+-- Appearance / UI
+vim.opt.number = true
+vim.opt.showmode = false     -- statusline does this
+vim.opt.cursorcolumn = false -- fix jumps
+vim.opt.signcolumn = "yes"   -- gutter never shifts
+vim.opt.termguicolors = true -- 24-bit color baby
+vim.opt.showtabline = 2      -- always show tabs
+vim.opt.winborder = "rounded"
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Completion popup menu
+vim.opt.pumheight = 10       -- max 10 items at a time
+vim.opt.pummaxwidth = 80
+vim.opt.pumborder = "rounded"
+vim.opt.completeopt = { "menuone", "popup", "nearest", "noinsert" }
+
+-- LSP / syntax highlights
+vim.cmd([[hi @lsp.type.number gui=bold]])
